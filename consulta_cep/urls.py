c03fda0d1e94b5dd9159c20f1125e8af6fb7b6b5
Sambiase <marsamb@gmail.com>
2023-01-28 11:58:02 -0300

    added new files

--- consulta_cep/urls.py
+++ consulta_cep/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import main_view, consulta_cep
+from .views import main_view, consulta_cep, modal
 
 urlpatterns = [
     path('', main_view, name='main_view'),
     path('consulta_cep/', consulta_cep, name='consulta_cep'),
-   
+    path('modal/', modal),
+
 
 ]
--- consulta_cep/views.py
+++ consulta_cep/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 
 
@@ -6,14 +6,24 @@ def main_view(request):
     return render(request, 'index.html')
 
 
+def modal(request):
+    return render(request, 'modal.html')
+
+
 def consulta_cep(request):
-    cep = request.POST.get('cep')
-    data = requests.get(f'https://viacep.com.br/ws/{cep}/json/').json()
-    res_list = [
-        data['logradouro'], data['bairro'],
-        data['localidade'], data['uf']
-    ]
-    context = {
-        'res_list': res_list
-    }
-    return render(request, 'index.html', context)
+    try:
+
+        cep = request.POST.get('cep')
+        data = requests.get(f'https://viacep.com.br/ws/{cep}/json/').json()
+
+        res_list = [
+            data['logradouro'], data['bairro'],
+            data['localidade'], data['uf']
+        ]
+        context = {
+            'res_list': res_list
+        }
+
+        return render(request, 'index.html', context)
+    except ValueError:
+        return redirect(main_view)
